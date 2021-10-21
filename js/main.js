617ae58e64c75d5e167c2743606ce4d5bc16956c
maksuedson.silva <maksuedson.silva@fornecedores.sicoob.com.br>
2021-10-21 00:47:12 -0300

    Capturando endereco do cep no console.log

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const pesquisarCep = () => {
+const pesquisarCep = async() => {
     const cep = document.getElementById('cep').value;
     const url = `http://viacep.com.br/ws/${cep}/json/`;
 
-    fetch(url).then(response => response.json())
-                                        .then(console.log);
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    console.log(endereco);
+
 }
 
 document.getElementById("cep")
