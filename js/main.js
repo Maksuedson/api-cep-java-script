8254b9ad13a4053915f76c0eca22a9b9d4ad31ab
maksuedson.silva <maksuedson.silva@fornecedores.sicoob.com.br>
2021-10-21 00:35:01 -0300

    script javaScript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const pesquisarCep = () => {
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url).then(response => response.json())
+                                        .then(console.log);
+}
+
+document.getElementById("cep")
+        .addEventListener('focusout', pesquisarCep);
