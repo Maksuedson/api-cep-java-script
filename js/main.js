3f228f4edc78bcf260ade4a08273b766633acffe
maksuedson.silva <maksuedson.silva@fornecedores.sicoob.com.br>
2021-10-22 21:29:12 -0300

    pronto

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
-'use strict';
+"use strict";
 
-const pesquisarCep = async() => {
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+const preencherFormulario = (endereco) => {
+  document.getElementById("endereco").value = endereco.logradouro;
+  document.getElementById("bairro").value = endereco.bairro;
+  document.getElementById("cidade").value = endereco.localidade;
+  document.getElementById("estado").value = endereco.uf;
+};
 
+const limparFormulario = (endereco) => {
+    document.getElementById("endereco").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("estado").value = "";
+  };
+
+
+const cepValido = (cep) => cep.length == 8 ;
+
+const pesquisarCep = async () => {
+    limparFormulario();
+    
+  const cep = document.getElementById("cep").value;
+  const url = `http://viacep.com.br/ws/${cep}/json/`;
+
+  if (cepValido(cep)) {
     const dados = await fetch(url);
     const endereco = await dados.json();
-    console.log(endereco);
 
-}
+    if (endereco.hasOwnProperty("erro")) {
+      document.getElementById("nome").value = "CEP não encontrado!";
+    } else {
+      preencherFormulario(endereco);
+    }
+  }
+};
 
-document.getElementById("cep")
-        .addEventListener('focusout', pesquisarCep);
+document.getElementById("cep").addEventListener("focusout", pesquisarCep);
